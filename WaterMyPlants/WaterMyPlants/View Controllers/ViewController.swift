39254cddd168f7df6683878f73ee7239a74ec62b
hai666l <0xconr@protonmail.com>
2020-06-16 22:20:45 -0500

    Created project, organized project files

---
+++ WaterMyPlants/WaterMyPlants/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WaterMyPlants
+//
+//  Created by conner on 6/16/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
