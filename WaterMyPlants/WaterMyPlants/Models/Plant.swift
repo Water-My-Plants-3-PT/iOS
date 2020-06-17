156167846462a3bcd14687ca7cedff1d8aebb504
hai666l <0xconr@protonmail.com>
2020-06-16 22:23:49 -0500

    Added a plant model

---
+++ WaterMyPlants/WaterMyPlants/Models/Plant.swift
@@ -0,0 +1,14 @@
+//
+//  Plant.swift
+//  WaterMyPlants
+//
+//  Created by conner on 6/16/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+import Foundation
+
+struct Plant {
+    var name: String
+    var lastWatered: Date
+}
