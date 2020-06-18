db621ecb2e1c8064e550017881adc1259376b705
Bronson <bronsonmullens@icloud.com>
2020-06-17 19:27:03 -0700

    Added detail VC

---
+++ WaterMyPlants/WaterMyPlants/View Controllers/PlantDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PlantDetailViewController.swift
+//  WaterMyPlants
+//
+//  Created by Bronson Mullens on 6/17/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+import UIKit
+
+class PlantDetailViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - IBOutlets
+    
+    // MARK: - IBActions
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
