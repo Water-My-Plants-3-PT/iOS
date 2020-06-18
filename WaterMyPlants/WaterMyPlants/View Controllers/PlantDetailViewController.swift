047aebe132632bb9ccb2efed31728daf7ad950b9
Bronson <bronsonmullens@icloud.com>
2020-06-17 19:54:57 -0700

    Set up some IBOutlets and stubbed func for date picker

--- WaterMyPlants/WaterMyPlants/View Controllers/PlantDetailViewController.swift
+++ WaterMyPlants/WaterMyPlants/View Controllers/PlantDetailViewController.swift
@@ -12,12 +12,34 @@ class PlantDetailViewController: UIViewController {
     
     // MARK: - Properties
     
+    // let plantController = PlantController
+    
+    let dateAdded = Date()
+    
     // MARK: - IBOutlets
+    
+    // Used to fetch the frequency of watering
+    @IBOutlet weak var frequencyPicker: UIDatePicker!
+    @IBOutlet weak var frequencyLabel: UILabel!
     
     // MARK: - IBActions
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Methods
+    
+    func frequencyPickerChanged(frequencyPicker: UIDatePicker) {
+        let dateFormatter = DateFormatter()
+        
+        /*
+         Should be able to grab whatever date is on the picker and
+         convert it to a string to be stored in the label's text.
+         Might be useful to also store the date to be used in a calculation
+         to determine a future watering date.
+         */
+        
     }
 
 }
